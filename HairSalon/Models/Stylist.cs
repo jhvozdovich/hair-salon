f82f9d3695e6da9b78ee99b5a0ed690311443671
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-29 09:57:11 -0700

    add client controller and index view

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -6,7 +6,7 @@ namespace HairSalon.Models
 {
   public class Stylist
   {
-    public int SalonId { get; set; }
+    public int StylistId { get; set; }
     public string Name { get; set; }
     public virtual ICollection<Client> Clients { get; set; }
     public Stylist()
